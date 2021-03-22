c46c6c89651cdd9e3b5224fbc19387fbe0c0ca97
EZIO-17 <samatreya@gmail.com>
2021-03-22 23:26:58 +0530

    first commit

--- src/header.jsx
+++ src/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
         <div className="header">
-        <img src="https://i.gadgets360cdn.com/large/google_keep_new_icon_web_1604497162908.jpg?downsize=950:*&output-quality=80" width='90' height='50'/>
+        <img src="https://i.gadgets360cdn.com/large/google_keep_new_icon_web_1604497162908.jpg?downsize=950:*&output-quality=80" width='90' height='50' alt="..."/>
         <h1> Google Keep</h1>
         </div>
         </>
